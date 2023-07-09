c3fb7fd5670cefb2032151a9aeaa138ac7aa629b
Ibrahim Fakharany <ibraheemelfakharany@gmail.com>
2023-07-09 10:05:47 +0300

    delete test from router

--- router/index.js
+++ router/index.js
@@ -1,7 +1,6 @@
 const productsRoute = require("./productRouter");
 
 const mountRoutes = (app) => {
-  // app.use("/api/v1/tests/", testRoute);
   app.use("/api/v1/products/", productsRoute);
 };
 module.exports = mountRoutes;
